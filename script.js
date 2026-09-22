43a4c1c82c3baf0ffa6aa07389ddda21c0744932
mimsud <solii20067@gmail.com>
2026-09-22 23:25:15 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-const addBtn = document.getElementById("addBtn");
+// حالت تاریک / روشن
+const themeBtn = document.getElementById("themeBtn");
 
-const num1 = document.querySelector('input[placeholder="عدد اول"]');
-const num2 = document.querySelector('input[placeholder="عدد دوم"]');
+themeBtn.addEventListener("click", function() {
+    document.body.classList.toggle("light-mode");
+});
 
-addBtn.addEventListener("click", function() {
 
-    const number1 = Number(num1.value);
-    const number2 = Number(num2.value);
-    const result = number1 + number2;
-    alert(result);
+// دکمه‌های اندازه متن
+const increaseText = document.getElementById("increaseText");
+const decreaseText = document.getElementById("decreaseText");
 
+let textSize = 16;
+
+
+// بزرگ‌تر کردن متن
+increaseText.addEventListener("click", function() {
+    textSize += 2;
+    document.body.style.fontSize = textSize + "px";
+});
+
+
+// کوچک‌تر کردن متن
+decreaseText.addEventListener("click", function() {
+    textSize -= 2;
+    document.body.style.fontSize = textSize + "px";
+});
+
+// بانشانی کارخانه
+const resetSettings = document.getElementById("resetSettings");
+resetSettings.addEventListener("click", function() {
+    document.body.classList.remove("light-mode");
+    textSize = 16;
+    document.body.style.fontSize = "16px";
 });
